00726ce37c64c69fe02a905ec192a76095c71ee7
thrombe <yash.n.thombare620@gmail.com>
2025-07-25 04:37:12 +0530

    disable no_llvm for better debugging :|

--- build.zig
+++ build.zig
@@ -16,10 +16,13 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "jjazy",
         .root_module = exe_mod,
-        .use_llvm = switch (optimize) {
-            .Debug => false,
-            else => null,
-        },
+
+        // debugger does not understand whatever zig generates
+        // very well yet :/
+        // .use_llvm = switch (optimize) {
+        //     .Debug => false,
+        //     else => null,
+        // },
     });
 
     b.installArtifact(exe);

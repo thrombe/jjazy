d54b1ada9a2dc85e192c8b61fb91f93f03790c65
thrombe <yash.n.thombare620@gmail.com>
2025-08-05 02:09:23 +0530

    build.zig allow tests in multiple files

--- build.zig
+++ build.zig
@@ -34,10 +34,15 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    const test_files = [_][]const u8{};
+    for (test_files) |file| {
+        const unit_tests = b.addTest(.{
+            .root_source_file = b.path(file),
+            .target = target,
+            .optimize = optimize,
+        });
+        const run_exe_unit_tests = b.addRunArtifact(unit_tests);
+        test_step.dependOn(&run_exe_unit_tests.step);
+    }
 }
